60a3b1b9af17e83dacd27d88b48832134344ce27
afif1400 <afifahmed456123@gmail.com>
2021-11-05 22:41:39 +0530

    📦 NEW: implemented database adapter

---
+++ domain/CustomerRepositoryDb.go
@@ -0,0 +1,48 @@
+package domain
+
+import (
+	"database/sql"
+	"log"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type CustomerRepositoryDb struct {
+	client *sql.DB
+}
+
+func (d CustomerRepositoryDb) FindAll() ([]Customer, error) {
+	findAllSql := "SELECT customer_id, name, city, zipcode, date_of_birth, status FROM customers"
+	rows, err := d.client.Query(findAllSql)
+
+	if err != nil {
+		log.Println("error while querying customer table" + err.Error())
+		return nil, err
+	}
+	var customers []Customer
+	for rows.Next() {
+		var customer Customer
+		err := rows.Scan(&customer.Id, &customer.Name, &customer.City, &customer.Zipcode, &customer.DateOfBirth, &customer.Status)
+		if err != nil {
+			log.Println("error while scanning customer table" + err.Error())
+			return nil, err
+		}
+		customers = append(customers, customer)
+	}
+
+	return customers, nil
+}
+
+func NewCustomerRepositoryDb() CustomerRepositoryDb {
+	client, err := sql.Open("mysql", "vulcan:password@tcp(localhost:3306)/banking")
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	client.SetConnMaxLifetime(time.Minute * 3)
+	client.SetMaxOpenConns(10)
+	client.SetMaxIdleConns(10)
+
+	return CustomerRepositoryDb{client}
+}
