afbbd2ed037489b5cafc128ec40adaefe23a926e
afif1400 <afifahmed456123@gmail.com>
2021-11-19 20:44:36 -0800

    👌 IMPROVE: added dto

--- service/customerService.go
+++ service/customerService.go
@@ -2,12 +2,13 @@ package service
 
 import (
 	"banking/domain"
+	"banking/dto"
 	"banking/err"
 )
 
 type CustomerService interface {
 	GetAllCustomer() ([]domain.Customer, *err.AppError)
-	GetCustomer(string) (*domain.Customer, *err.AppError)
+	GetCustomer(string) (*dto.CustomerResponse, *err.AppError)
 	GetAllCustomerByStatus(string) ([]domain.Customer, *err.AppError)
 }
 
@@ -23,8 +24,23 @@ func (s DefaultCustomerService) GetAllCustomerByStatus(status string) ([]domain.
 	return s.repo.FindAllByStatus(status)
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *err.AppError) {
-	return s.repo.ById(id)
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *err.AppError) {
+	customer, errs := s.repo.ById(id)
+
+	if errs != nil {
+		return nil, errs
+	}
+
+	response := dto.CustomerResponse{
+		Id:          customer.Id,
+		Name:        customer.Name,
+		City:        customer.City,
+		Zipcode:     customer.Zipcode,
+		DateOfBirth: customer.DateOfBirth,
+		Status:      customer.Status,
+	}
+
+	return &response, nil
 }
 
 func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
